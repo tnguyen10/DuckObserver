39907a028c25f2ee5bda02aefed7dab8014e5bcf
tnguyen10 <tnguyen1@ualberta.ca>
2022-01-24 16:34:35 -0700

    feat: connect to local mongodb, set api routes for get and post

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 app.use(helmet());
@@ -10,14 +11,31 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("combined"));
 
+let database = null;
+const MONGODB_URL = "mongodb://127.0.0.1:27017"
+const MONGODB_COLLECTION = "DUCK_OBSERVER"
+
+async function connectToDatabase() {
+  const mongoDBURL = MONGODB_URL;
+  const connection = await MongoClient.connect(mongoDBURL, {
+    useNewUrlParser: true,
+  });
+  database = connection.db();
+}
+
 app.post("/", async (req, res) => {
-  res.send({ message: "POST" });
+  const data = req.body;
+  data && (await database.collection(MONGODB_COLLECTION).insertOne(data));
+  res.send({ status: "200", message: "Send success" });
 });
 
-app.get("/", async (req, res) =>  {
-    res.send({ message: "GET" });
+app.get("/", async (req, res) => {
+  const data = await database.collection(MONGODB_COLLECTION).find({}).toArray();
+  res.send({ status: "200", message: "Request success", data: data });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+connectToDatabase().then(async () => {
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
 });
